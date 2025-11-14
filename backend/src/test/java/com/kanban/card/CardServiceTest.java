26b339c692adfc9ee115b092a8a795110cfe6105
mzd-hseokkim <hseokkim@mz.co.kr>
2025-11-14 10:28:17 +0900

    Remove unused imports

--- backend/src/test/java/com/kanban/card/CardServiceTest.java
+++ backend/src/test/java/com/kanban/card/CardServiceTest.java
@@ -1,13 +1,13 @@
 package com.kanban.card;
 
-import com.kanban.activity.ActivityService;
-import com.kanban.board.member.BoardMemberRoleValidator;
-import com.kanban.card.dto.CreateCardRequest;
-import com.kanban.card.dto.UpdateCardRequest;
-import com.kanban.column.BoardColumn;
-import com.kanban.column.ColumnRepository;
-import com.kanban.label.CardLabelRepository;
-import com.kanban.user.UserRepository;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,12 +17,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.owasp.html.PolicyFactory;
 
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.Mockito.*;
+import com.kanban.activity.ActivityService;
+import com.kanban.board.member.BoardMemberRoleValidator;
+import com.kanban.card.dto.CreateCardRequest;
+import com.kanban.card.dto.UpdateCardRequest;
+import com.kanban.column.BoardColumn;
+import com.kanban.column.ColumnRepository;
+import com.kanban.label.CardLabelRepository;
+import com.kanban.user.UserRepository;
 
 /**
  * CardService Unit Test
